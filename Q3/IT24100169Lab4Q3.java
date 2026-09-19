e2357f06aac9ac2ea20f555cb207e8008683b066
Tharushi15 <rmtdrajaguru@gmail.com>
2026-09-19 09:55:21 +0530

    IT24100169_Lab4_Q3

---
+++ Q3/IT24100169Lab4Q3.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class IT24100169Lab4Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        String result = (number > 0) ? "Positive" :
+                        (number < 0) ? "Negative" : "Zero";
+
+        System.out.println("The number is: " + result);
+
+        
+    }
+}
